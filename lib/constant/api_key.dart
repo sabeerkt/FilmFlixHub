4c87b1bddd3be784400e8ddf3dfd13586fef6e09
sabeer <mohammedsabeer.kt@gmail.com>
2024-01-01 10:05:36 +0530

    api

--- lib/constant/api_key.dart
+++ lib/constant/api_key.dart
@@ -7,7 +7,7 @@ class Constants {
   static const imagepath = 'https://image.tmdb.org/t/p/w500';
 
   static const trendingurl =
-      'https://api.themoviedb.org/3/trending/movie/day?api_key=aa9a8a205c0591e06a5292b3c1835f3a';
+      'https://api.themoviedb.org/3/trending/movie/day?api_key=$apiKey';
   static const show =
       'https://api.themoviedb.org/3/tv/popular?api_key=aa9a8a205c0591e06a5292b3c1835f3a';
   static const airtvshow =
